09894e82ddc6250a460c35285be0a75c746e8b11
lwwdaehwan <dleoghks1030@gmail.com>
2024-07-01 17:29:38 +0900

    work_control

---
+++ control/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+# Create your views here.
+
+def control(request):
+    return render(request, 'control.html')
