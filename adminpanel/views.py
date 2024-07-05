71506fa92b522b6245cca088ed470770d8db952d
doyoung <mintehfl@naver.com>
2024-07-05 16:42:18 +0900

    feat: 관리자(훈련시나리오) csv 파일 업로드, DB 저장 및 삭제

--- adminpanel/views.py
+++ adminpanel/views.py
@@ -14,14 +14,66 @@ from main.models import Event_board, Notice_board
 from django.contrib import messages
 import json
 from main.forms import *
+import csv
+import io
+from datetime import datetime
+from main.utils import similarity_function 
 
 def admin_home(request):
     return render(request, 'adminpanel/admin_home.html')
 
 def senario(request):
-    scenario = Scenario.objects.all()
-    ## 이후 작업 필요
-    return render(request, 'adminpanel/senario.html')
+    scenario_list = Scenario.objects.all()
+    paginator = Paginator(scenario_list, 10)  # Show 10 scenarios per page
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'adminpanel/senario.html', {'page_obj': page_obj})
+
+def csv_upload(request):
+    if request.method == 'POST':
+        csv_file = request.FILES['csv_file']
+        if not csv_file.name.endswith('.csv'):
+            # 유효하지 않은 파일 유형 처리
+            return render(request, 'csv_upload.html', {'error': 'CSV 파일이 아닙니다.'})
+        
+        file_data = csv_file.read().decode('utf-8-sig')  # 'utf-8-sig'로 BOM 처리
+        csv_reader = csv.reader(io.StringIO(file_data))
+        
+        next(csv_reader)  # 첫 번째 행(헤더) 건너뛰기
+        
+        for row in csv_reader:
+            if len(row) < 6:  # 모델의 필드 수에 맞게 조정
+                continue
+            
+            try:
+                # '시간' 데이터를 적절한 datetime 형식으로 변환
+                scenario_time = datetime.strptime(row[2], '%H:%M')
+            except ValueError:
+                # 변환할 수 없는 경우 건너뜀
+                continue
+            
+            Scenario.objects.create(
+                scenario_code=row[1],
+                scenario_time=scenario_time,
+                scenario_situation=row[3],
+                scenario_process=row[4],
+                scenario_goals=row[5],
+            )
+        
+        return redirect('adminpanel:senario')
+    
+    return render(request, 'adminpanel/csv_upload.html')
+
+def delete_senario(request):
+    try:
+        data = json.loads(request.body)
+        ids_to_delete = data['ids']
+        Scenario.objects.filter(scenario_id__in=ids_to_delete).delete()
+        return JsonResponse({"status": "success"}, status=200)
+    except Exception as e:
+        return JsonResponse({"status": "error", "message": str(e)}, status=400)
 
 def board_manage(request):
     posts = Event_board.objects.all().order_by('-event_wdate')
