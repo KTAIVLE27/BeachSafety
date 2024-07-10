4555b1e245d5dbc62bdd5cd5000bda6cf3e88c4b
seri <tpfl10048@naver.com>
2024-07-10 17:04:19 +0900

    feat: api job schedul

--- control/utils.py
+++ control/utils.py
@@ -2,53 +2,95 @@ import requests
 import os
 from main.models import Beach
 # 환경 변수에서 API 키를 가져옵니다.
-RIP_API_KEY = os.getenv('RIP_API_KEY')
+# RIP_API_KEY = os.getenv('RIP_API_KEY')
 
-url = f'http://www.khoa.go.kr/api/oceangrid/ripCurrentRecent/search.do?ServiceKey={RIP_API_KEY}&ResultType=json'
+# url = f'http://www.khoa.go.kr/api/oceangrid/ripCurrentRecent/search.do?ServiceKey={RIP_API_KEY}&ResultType=json'
 
-response = requests.get(url)
-contents = response.text
+# response = requests.get(url)
+# contents = response.text
 
-if response.status_code == 200:
-    # JSON 데이터 파싱
-    data = response.json()
+# if response.status_code == 200:
+#     # JSON 데이터 파싱
+#     data = response.json()
     
-    # 결과 데이터를 저장할 사전
-    beach_data_dict = {}
+#     # 결과 데이터를 저장할 사전
+#     beach_data_dict = {}
     
-    for beach in data['result']['data']:
-        beach_code = beach['beach_code']
-        beach_data_dict[beach_code] = {
-            #'beach_name': beach['beach_name'],
-            'obs_time': beach['obs_time'],
-            #'water_temp': beach['water_temp'],
-            #'air_temp': beach['air_temp'],
-            #'wind_speed': beach['wind_speed'],
-            'lon': beach['lon'],
-            'lat': beach['lat'],
-            #'wind_direct': beach['wind_direct'],
-            #'wave_height': beach['wave_height'],
-            #'wave_period': beach['wave_period'],
-            'score_msg': beach['score_msg'],
-            #'score': beach['score'],
-        }
+#     for beach in data['result']['data']:
+#         beach_code = beach['beach_code']
+#         beach_data_dict[beach_code] = {
+#             #'beach_name': beach['beach_name'],
+#             'obs_time': beach['obs_time'],
+#             #'water_temp': beach['water_temp'],
+#             #'air_temp': beach['air_temp'],
+#             #'wind_speed': beach['wind_speed'],
+#             'lon': beach['lon'],
+#             'lat': beach['lat'],
+#             #'wind_direct': beach['wind_direct'],
+#             #'wave_height': beach['wave_height'],
+#             #'wave_period': beach['wave_period'],
+#             'score_msg': beach['score_msg'],
+#             #'score': beach['score'],
+#         }
     
-    #결과 출력
-    # for beach_code, details in beach_data_dict.items():
-    #     print(f"Beach Code: {beach_code}")
-    #     for key, value in details.items():
-    #         print(f"{key}: {value}")
-    #     print("\n")
+#     #결과 출력
+#     # for beach_code, details in beach_data_dict.items():
+#     #     print(f"Beach Code: {beach_code}")
+#     #     for key, value in details.items():
+#     #         print(f"{key}: {value}")
+#     #     print("\n")
     
-    #print(beach_data_dict['HAE']['lon'])
-else:
-    print(f"Failed to retrieve data: {response.status_code}")
+#     #print(beach_data_dict['HAE']['lon'])
+# else:
+#     print(f"Failed to retrieve data: {response.status_code}")
     
 # ['GYEONGPO', 'GORAEBUL','NAKSAN','DAECHON','MANGSANG','SOKCHO','SONGJUNG','IMRANG','JUNGMUN','HAE']
+
+
+
+def fetch_rip_current_data():
+    # 환경 변수에서 API 키를 가져옵니다.
+    api_key = os.getenv('RIP_API_KEY')
+    url = f'http://www.khoa.go.kr/api/oceangrid/ripCurrentRecent/search.do?ServiceKey={api_key}&ResultType=json'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        # JSON 데이터 파싱
+        data = response.json()
+        
+        # 결과 데이터를 저장할 사전
+        beach_data_dict = {}
+        
+        for beach in data['result']['data']:
+            beach_code = beach['beach_code']
+            beach_data_dict[beach_code] = {
+                #'beach_name': beach['beach_name'],
+                'obs_time': beach['obs_time'],
+                #'water_temp': beach['water_temp'],
+                #'air_temp': beach['air_temp'],
+                #'wind_speed': beach['wind_speed'],
+                'lon': beach['lon'],
+                'lat': beach['lat'],
+                #'wind_direct': beach['wind_direct'],
+                #'wave_height': beach['wave_height'],
+                #'wave_period': beach['wave_period'],
+                'score_msg': beach['score_msg'],
+                #'score': beach['score'],
+            }
+        print(beach_data_dict['HAE'])
+        return beach_data_dict
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
+        return None
+
+
+
 
 #이안류 위험 지수
 def get_beach_score_msg(beach_API_CODE):  
     try:
+        print('get_beach_score_msg')
+        beach_data_dict = fetch_rip_current_data()
         return beach_data_dict[beach_API_CODE]['score_msg']
     except KeyError:
         return "NaN"  # 값이 없을 때 반환할 기본 메시지
@@ -70,6 +112,8 @@ def get_beach_lat(beach_name):
     
 def get_beach_obs_time(beach_API_CODE):
     try:
+        beach_data_dict = fetch_rip_current_data()
         return beach_data_dict[beach_API_CODE]['obs_time']
     except KeyError:
         return "NaN"  # 값이 없을 때 반환할 기본 메시지
+    
--- myapp/settings.py
+++ myapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'main',
     'control',
     'adminpanel',
+    'django_apscheduler',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,7 @@ AUTH_USER_MODEL = 'main.User'
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/'
+
+# APScheduler 설정
+APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"  # 디폴트: "N j, Y, f:s a"
+APSCHEDULER_RUN_NOW_TIMEOUT = 30  # 디폴트: 25초
