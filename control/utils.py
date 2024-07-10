4dd646abcf4213a2e0061344f5571222e8f06fd0
seri <tpfl10048@naver.com>
2024-07-10 18:07:14 +0900

    feat: 비동기 중간저장

--- control/utils.py
+++ control/utils.py
@@ -1,52 +1,6 @@
 import requests
 import os
 from main.models import Beach
-# 환경 변수에서 API 키를 가져옵니다.
-# RIP_API_KEY = os.getenv('RIP_API_KEY')
-
-# url = f'http://www.khoa.go.kr/api/oceangrid/ripCurrentRecent/search.do?ServiceKey={RIP_API_KEY}&ResultType=json'
-
-# response = requests.get(url)
-# contents = response.text
-
-# if response.status_code == 200:
-#     # JSON 데이터 파싱
-#     data = response.json()
-    
-#     # 결과 데이터를 저장할 사전
-#     beach_data_dict = {}
-    
-#     for beach in data['result']['data']:
-#         beach_code = beach['beach_code']
-#         beach_data_dict[beach_code] = {
-#             #'beach_name': beach['beach_name'],
-#             'obs_time': beach['obs_time'],
-#             #'water_temp': beach['water_temp'],
-#             #'air_temp': beach['air_temp'],
-#             #'wind_speed': beach['wind_speed'],
-#             'lon': beach['lon'],
-#             'lat': beach['lat'],
-#             #'wind_direct': beach['wind_direct'],
-#             #'wave_height': beach['wave_height'],
-#             #'wave_period': beach['wave_period'],
-#             'score_msg': beach['score_msg'],
-#             #'score': beach['score'],
-#         }
-    
-#     #결과 출력
-#     # for beach_code, details in beach_data_dict.items():
-#     #     print(f"Beach Code: {beach_code}")
-#     #     for key, value in details.items():
-#     #         print(f"{key}: {value}")
-#     #     print("\n")
-    
-#     #print(beach_data_dict['HAE']['lon'])
-# else:
-#     print(f"Failed to retrieve data: {response.status_code}")
-    
-# ['GYEONGPO', 'GORAEBUL','NAKSAN','DAECHON','MANGSANG','SOKCHO','SONGJUNG','IMRANG','JUNGMUN','HAE']
-
-
 
 def fetch_rip_current_data():
     # 환경 변수에서 API 키를 가져옵니다.
