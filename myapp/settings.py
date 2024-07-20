a64e65512b74658a81c66f9fedfd258f72bbc778
seohyeon <kgustjgus@naver.com>
2024-07-20 20:31:10 +0900

    Feat : settings.py 수정

--- myapp/settings.py
+++ myapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.38.173.10']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '15.165.7.128']
 
 # Application definition
 
